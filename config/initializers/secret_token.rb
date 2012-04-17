61d1097a740597c318209d515df147b1055c5269
Julio Budal <juli0w@hotmail.com>
2012-04-17 16:59:04 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NFe::Application.config.secret_token = '0b03a12476077f43c215c192e53f46dc3f1788bdba03e3a3e908693c63ba3d9ce3b1f19104f0105d8684094e19cd2861b956c247d0db92895bf5c1526b60c3e3'
